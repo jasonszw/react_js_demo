eee4b57e28c6fe192e85d4b5a4a6682cf82a672d
jason.shi <jason.shi@pixelplus.co>
2019-12-13 18:03:27 +0800

    更新

--- src/router.js
+++ src/router.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './App';
+import Home from './views/home'
 
+import {Router, Route} from "react-router";
+
+const routes = {
+    path: '/',
+    component: App,
+    childRoutes: [
+        { path: 'home', component: Home },
+    ]
+};
+Router.render(<Router routes={routes} />,document.body);
